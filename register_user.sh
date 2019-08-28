44f6eec4b75a1c9d0ba568851f7e628b25d1068e
gilpazo <il28@riseup.net>
2019-08-28 14:49:31 -0700

    tested register_user.sh on live server

--- register_user.sh
+++ register_user.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 #arguments: 1 username 2 password 
 register_new_matrix_user -c /etc/matrix-synapse/homeserver.yaml http://localhost:8008 --no-admin -u $1 -p $2
-#note that this is untested ^
 registration_exit_code=$?
 echo $registration_exit_code
 if [ $registration_exit_code != 0 ]
